1dafb7dce7d215c264aaf55c36b4ba626bb85eca
Matheus G <mgvr@poli.br>
2026-06-28 20:24:57 -0300

    Correção dos calculos de 51ABC e adição do requisito de 51V

--- relay_51fase_AT.py
+++ relay_51fase_AT.py
@@ -12,22 +12,32 @@ beta = 0.14
 
 # Tempo de atuação para proteção instantânea igual à 0.5 segundos
 t_instant = 0.5
-
 # Intervalo de tempo para coordenação definido como 0.4 segundos
 cti = 0.4
-
 fator_sobrecarga = 1.4
-
 # Curto trifásico na barra de 69kV nos fornece a corrente máxima em 230kV
 icc_max_230kV = 1434.6
 
-i_pickup = 1.4 * i_nominal_230kV
+i_sobrecarga = 1.4 * i_nominal_230kV
+print(f"Sobrecarga = {i_sobrecarga}")
+
+icc_min_230kV = 124.6 # Curto Bifásico na barra de 230kV
+i_pickup = 120 # Pois deve ser sensível ao menor curto (124.6 A)!
+print(f"Pickup = {i_pickup} \n")
+
+if i_pickup < i_sobrecarga: 
+    print(f"PRECISAREMOS DE 51 V EM SÉRIE, POIS A CORRENTE DO MENOR CURTO OBSERVADO ({icc_min_230kV} A) "
+          f"FAZ COM QUE SEJA NECESSÁRIO A DEFINIÇÃO DE UMA CORRENTE DE PICK-UP ({i_pickup} A) "
+          f"MENOR DO QUE A CORRENTE OBSERVADA SOBRE SOBRECARGA ({i_sobrecarga} A)\n")
 
 multiplo_sensi =  icc_max_230kV / i_pickup
 if multiplo_sensi < 2: 
-    sys.exit("Erro, múltiplo de sensibilidade está abaixo do esperado!")
+    sys.exit("Erro, múltiplo de sensibilidade está abaixo do esperado!\n")
 t_51_fase_230kV = t_instant + cti
 
+
+
 tms_51_fase_230kV = t_51_fase_230kV * (multiplo_sensi**alfa - 1) / beta
-print(f"Usando pickup PRIMÁRIO para a função 51 fase de 230kV igual à {round(i_pickup, 3)} A")
-print(f"tms para a função 51 fase de 230 kV: {round(tms_51_fase_230kV, 3)}")
+print(f"Encontramos um múltiplo de sensibilidade igual a: {multiplo_sensi}")
+print(f"Usando pickup PRIMÁRIO para a função 51 fase do relé de 230kV igual à {round(i_pickup, 3)} A")
+print(f"TMS para a função 51 fase de 230 kV: {round(tms_51_fase_230kV, 3)}\n")
--- relay_51fase_BT.py
+++ relay_51fase_BT.py
@@ -21,7 +21,17 @@ fator_sobrecarga = 1.4
 # Curto trifásico na barra de 69kV nos fornece a corrente máxima em 69kV
 icc_max_69kV = 4781.9
 
-i_pickup = 1.4 * i_nominal_69kV
+i_sobrecarga = 1.4 * i_nominal_69kV
+print(f"Sobrecarga = {i_sobrecarga}")
+
+icc_min_69kV = 239.2 # Curto Bifásico na barra de 230kV
+i_pickup = 220 # Pois deve ser sensível ao menor curto (239.2 A)!
+print(f"Pickup = {i_pickup}\n")
+
+if i_pickup < i_sobrecarga: 
+    print(f"PRECISAREMOS DE 51 V EM SÉRIE, POIS A CORRENTE DO MENOR CURTO OBSERVADO ({icc_min_69kV} A) "
+          f"FAZ COM QUE SEJA NECESSÁRIO A DEFINIÇÃO DE UMA CORRENTE DE PICK-UP ({i_pickup} A) "
+          f"MENOR DO QUE A CORRENTE OBSERVADA SOBRE SOBRECARGA ({i_sobrecarga} A)\n")
 
 multiplo_sensi =  icc_max_69kV / i_pickup
 if multiplo_sensi < 2: 
@@ -29,5 +39,6 @@ if multiplo_sensi < 2:
 t_51_fase_69kV = t_instant + cti
 
 tms_51_fase_69kV = t_51_fase_69kV * (multiplo_sensi**alfa - 1) / beta
-print(f"Usando pickup PRIMÁRIO para a função 51 fase de 230kV igual à {round(i_pickup, 3)} A")
-print(f"tms para a função 51 fase de kV: {round(tms_51_fase_69kV, 3)}")
+print(f"Encontramos um múltiplo de sensibilidade igual a: {multiplo_sensi}")
+print(f"Usando pickup PRIMÁRIO para a função 51 de fase do relé de 230kV igual à: {round(i_pickup, 3)} A")
+print(f"TMS para a função 51 fase de kV: {round(tms_51_fase_69kV, 3)}\n")
